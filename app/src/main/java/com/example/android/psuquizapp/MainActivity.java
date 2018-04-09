64678be683c5d74910f7611298a4879705c01879
John McGuigan <jrm5100@gmail.com>
2018-04-08 20:29:09 -0400

    All 10 questions done.

--- app/src/main/java/com/example/android/psuquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/psuquizapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.android.psuquizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
@@ -72,16 +71,70 @@ public class MainActivity extends AppCompatActivity {
         /* Question 3 */
         /* Farmer's High School of Pennsylvania */
         RadioGroup answerQ3rbg = findViewById(R.id.answer_q3);
-        RadioButton selected = answerQ3rbg.findViewById(answerQ3rbg.getCheckedRadioButtonId());
-        if (selected != null) { // Selected id is null if no choice was made, so it isn't possible to get text
-            String answerQ3 = (String) selected.getText();
+        RadioButton selectedQ3 = answerQ3rbg.findViewById(answerQ3rbg.getCheckedRadioButtonId());
+        if (selectedQ3 != null) { // Selected id is null if no choice was made, so it isn't possible to get text
+            String answerQ3 = (String) selectedQ3.getText();
             if (Objects.equals(answerQ3, "Farmer's High School of Pennsylvania")) {
                 score += 1;
             }
         }
 
         /* Question 4 */
+        /* 409 */
+        EditText answerQ4 = findViewById(R.id.answer_q4);
+        if(Objects.equals(answerQ4.getText().toString(), "409")){ score += 1;}
 
+        /* Question 5 */
+        /* Mule */
+        RadioGroup answerQ5rbg = findViewById(R.id.answer_q5);
+        RadioButton selectedQ5 = answerQ5rbg.findViewById(answerQ5rbg.getCheckedRadioButtonId());
+        if (selectedQ5 != null) { // Selected id is null if no choice was made, so it isn't possible to get text
+            String answerQ5 = (String) selectedQ5.getText();
+            if (Objects.equals(answerQ5, "Mule")) {
+                score += 1;
+            }
+        }
+
+        /* Question 6 */
+        /* Ensure only the last 2 options are checked for a correct answer */
+        CheckBox answerQ6a = findViewById(R.id.answer_q6_a);
+        CheckBox answerQ6b = findViewById(R.id.answer_q6_b);
+        CheckBox answerQ6c = findViewById(R.id.answer_q6_c);
+        CheckBox answerQ6d = findViewById(R.id.answer_q6_d);
+
+        if (!answerQ6a.isChecked() & !answerQ6b.isChecked() & answerQ6c.isChecked() & answerQ6d.isChecked()) {
+            score += 1;
+        }
+
+        /* Question 7 */
+        /* Old Main */
+        EditText answerQ7 = findViewById(R.id.answer_q7);
+        if(Objects.equals(answerQ7.getText().toString().toLowerCase(), "old main")){ score += 1;}
+
+        /* Question 8 */
+        /* 2 */
+        EditText answerQ8 = findViewById(R.id.answer_q8);
+        if(Objects.equals(answerQ8.getText().toString(), "2")){ score += 1;}
+
+        /* Question 9 */
+        /* The Berkey Creamery */
+        RadioGroup answerQ9rbg = findViewById(R.id.answer_q9);
+        RadioButton selectedQ9 = answerQ9rbg.findViewById(answerQ9rbg.getCheckedRadioButtonId());
+        if (selectedQ9 != null) { // Selected id is null if no choice was made, so it isn't possible to get text
+            String answerQ9 = (String) selectedQ9.getText();
+            if (Objects.equals(answerQ9, "The Berkey Creamery")) {
+                score += 1;
+            }
+        }
+
+        /* Question 10 */
+        /* "Penn State" or "Penn State!" */
+        EditText answerQ10 = findViewById(R.id.answer_q10);
+        if (Objects.equals(answerQ10.getText().toString().toLowerCase(), "penn state")) {
+            score += 1;
+        } else if (Objects.equals(answerQ10.getText().toString().toLowerCase(), "penn state!")) {
+            score += 1;
+        }
 
         return score;
     }
